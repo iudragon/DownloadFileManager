8d20ca796ce38a4ed3a04929e5ac37a410bb4ddd
iudragon <42891947+TeamLewok@users.noreply.github.com>
2019-01-25 23:09:10 +0530

    ki

--- app/src/main/java/dragon/bakuman/iu/downloadfilemanager/MainActivity.java
+++ app/src/main/java/dragon/bakuman/iu/downloadfilemanager/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.database.Cursor;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,17 +15,40 @@ import android.view.View;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
 
     long queueId;
     DownloadManager dm;
-
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        /* MUSIC */
+
+        String url = "http://feeds.soundcloud.com/stream/550150548-iu-dragon-summer-upbeat.mp3"; // your URL here
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        try {
+            mediaPlayer.setDataSource(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            mediaPlayer.prepare(); // might take long! (for buffering, etc)
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        /* MUSIC */
 
 
         BroadcastReceiver receiver = new BroadcastReceiver() {
@@ -78,10 +103,28 @@ public class MainActivity extends AppCompatActivity {
     public void downloadClick(View view) {
 
         dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
-        DownloadManager.Request request = new DownloadManager.Request(Uri.parse("http://appeteria.com/video.mp4"));
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse("http://feeds.soundcloud.com/stream/550150548-iu-dragon-summer-upbeat.mp3"));
 
         queueId = dm.enqueue(request);
 
 
     }
+
+    public void playClick(View view) {
+
+        mediaPlayer.start();
+
+    }
+
+    public void pauseClick(View view) {
+        mediaPlayer.pause();
+
+    }
+
+    public void stopClick(View view) {
+
+        mediaPlayer.stop();
+    }
+
+
 }
